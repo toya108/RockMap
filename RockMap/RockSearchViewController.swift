8449aef527c6b9c52606dc834f9e79f8f689e689
kawano108 <tk108gabalian@gmail.com>
2020-10-26 00:15:19 +0900

    mapにピン立てる処理を試しで追加

--- RockMap/RockSearchViewController.swift
+++ RockMap/RockSearchViewController.swift
@@ -6,9 +6,13 @@
 //
 
 import UIKit
+import FirebaseFirestore
+import MapKit
 
 final class RockSearchViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     private lazy var searchBar: UISearchBar = {
         let bar = UISearchBar()
         bar.placeholder = "岩の名前で探す"
@@ -19,6 +23,16 @@ final class RockSearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupSearchBar()
+        
+        let coordinate = CLLocationCoordinate2DMake(35.801128, 139.175756)
+        let span = MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinate
+        pin.title = "デッドエンド岩"
+        mapView.addAnnotation(pin)
     }
     
     func setupSearchBar() {
