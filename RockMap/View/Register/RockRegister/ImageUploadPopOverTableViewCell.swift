b4d9e5212e59c986d7415ba271cafd510c1bd186
kawano108 <tk108gabalian@gmail.com>
2020-11-05 01:47:40 +0900

    写真選択用のpopupを作成

---
+++ RockMap/View/Register/RockRegister/ImageUploadPopOverTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ImageUploadPopOverTableViewCell.swift
+//  RockMap
+//
+//  Created by TOUYA KAWANO on 2020/11/05.
+//
+
+import UIKit
+
+class ImageUploadPopOverTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var howToUploadImageTitleLabel: UILabel!
+    @IBOutlet weak var howToUploadImageIconView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ RockMap/View/Register/RockRegister/ImageUploadPopOverTableViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ImageUploadPopOverTableViewController.swift
+//  RockMap
+//
+//  Created by TOUYA KAWANO on 2020/11/05.
+//
+
+import UIKit
+
+class ImageUploadPopOverTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.isScrollEnabled = false
+        tableView.register(UINib(nibName: ImageUploadPopOverTableViewCell.className, bundle: nil), forCellReuseIdentifier: ImageUploadPopOverTableViewCell.className)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ImageUploadPopOverTableViewCell.className, for: indexPath) as? ImageUploadPopOverTableViewCell else { return UITableViewCell() }
+        
+        switch indexPath.row {
+        case 0:
+            cell.howToUploadImageTitleLabel?.text = "フォトライブラリ"
+            cell.howToUploadImageIconView.image = UIImage(systemName: "folder.fill")
+            
+        case 1:
+            cell.howToUploadImageTitleLabel?.text = "写真を撮る"
+            cell.howToUploadImageIconView.image = UIImage(systemName: "camera.fill")
+            
+        default:
+            break
+        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        switch indexPath.row {
+        case 0:
+
+        case 1:
+            break
+            
+        default:
+            break
+        }
+    }
+}
