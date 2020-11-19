e5927f2814b05ea34fe628803fd7dd952923ab09
kawano108 <tk108gabalian@gmail.com>
2020-11-19 23:11:24 +0900

    RockDetailMapにoutletを追加

--- RockMap/View/RockDetail/RockDetailMapViewController.swift
+++ RockMap/View/RockDetail/RockDetailMapViewController.swift
@@ -6,25 +6,16 @@
 //
 
 import UIKit
+import MapKit
 
 class RockDetailMapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var addressLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .red
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
