c7e788281a09093a6967254331d112d7903a891d
kawano108 <tk108gabalian@gmail.com>
2021-01-27 23:29:15 +0900

    #21 岩のリファレンス一覧取得とリファレンスに応じた画像取得を実装

--- RockMap/Model/Firebase/Storage/StorageManager.swift
+++ RockMap/Model/Firebase/Storage/StorageManager.swift
@@ -13,4 +13,32 @@ struct StorageManager {
     static func makeReference(parent: FINameSpaceProtocol.Type, child: String) -> StorageReference {
         Self.reference.child(parent.name).child(child)
     }
+    
+    static func getAllImageReference(reference: StorageReference, completion: @escaping (Result<[StorageReference], Error>) -> Void) {
+        reference.listAll { result, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            completion(.success(result.items))
+        }
+    }
+    
+    static func getImage(reference: StorageReference, completion: @escaping (Result<Data, Error>) -> Void) {
+        reference.getData(maxSize: 1 * 1024 * 1024) { data, error in
+            
+            if let error = error {
+                print("画像のDLに失敗しました\(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else {
+                print("画像のdataを取得できませんでした。")
+                return
+            }
+            
+            completion(.success(data))
+        }
+    }
 }
