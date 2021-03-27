b9ec6e238bc2b7406f10029aca88d3c6b25afbba
kawano108 <tk108gabalian@gmail.com>
2021-03-28 02:51:28 +0900

    #27 ファイル構成を変更

--- RockMap/View/RockSearch/RockSearchViewController.swift
+++ RockMap/View/RockSearch/RockSearchViewController.swift
@@ -380,23 +380,6 @@ extension RockSearchViewController: MKMapViewDelegate {
     }
 }
 
-class RockAnnotation: NSObject, MKAnnotation {
-    
-    let rock: FIDocument.Rock
-    let coordinate: CLLocationCoordinate2D
-    var title: String?
-
-    init(
-        coordinate: CLLocationCoordinate2D,
-        rock: FIDocument.Rock,
-        title: String
-    ) {
-        self.rock = rock
-        self.coordinate = coordinate
-        self.title = title
-    }
-}
-
 extension RockSearchViewController: FloatingPanelControllerDelegate {
 
     func floatingPanel(
