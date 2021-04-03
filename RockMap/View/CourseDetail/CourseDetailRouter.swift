57d3ff36aa86b0be20685e7c2a20c54468db3c24
kawano108 <tk108gabalian@gmail.com>
2021-04-03 23:38:37 +0900

    #94 課題詳細のRouterを作成

---
+++ RockMap/View/CourseDetail/CourseDetailRouter.swift
@@ -0,0 +1,86 @@
+//
+//  CourseDetailRouter.swift
+//  RockMap
+//
+//  Created by TOUYA KAWANO on 2021/04/03.
+//
+
+import UIKit
+
+struct CourseDetailRouter: RouterProtocol {
+
+    typealias Destination = DestinationType
+
+    enum DestinationType: DestinationProtocol {
+        case registerClimbed
+    }
+
+    private weak var viewModel: CourseDetailViewModel!
+
+    init(viewModel: CourseDetailViewModel) {
+        self.viewModel = viewModel
+    }
+
+    func route(
+        to destination: Destination,
+        from: UIViewController
+    ) {
+        switch destination {
+            case .registerClimbed:
+                presentRegisterClimbedBottomSheet(from)
+
+        }
+    }
+
+    private func pushCourseDetail(
+        _ from: UIViewController,
+        course: FIDocument.Course
+    ) {
+        let courseDetailViewModel = CourseDetailViewModel(course: course)
+        from.navigationController?.pushViewController(
+            CourseDetailViewController.createInstance(viewModel: courseDetailViewModel),
+            animated: true
+        )
+    }
+
+    private func presentRegisterClimbedBottomSheet(_ from: UIViewController) {
+        let vc = RegisterClimbedBottomSheetViewController()
+
+        let recodeButtonAction: UIAction = .init { _ in
+
+            guard
+                let type = FIDocument.Climbed.ClimbedRecordType.allCases.any(
+                    at: vc.climbedTypeSegmentedControl.selectedSegmentIndex
+                )
+            else {
+                return
+            }
+
+            vc.showIndicatorView()
+
+            self.viewModel.registerClimbed(
+                climbedDate: vc.climbedDatePicker.date,
+                type: type
+            ) { result in
+
+                defer {
+                    vc.hideIndicatorView()
+                }
+
+                switch result {
+                    case .success:
+                        from.dismiss(animated: true)
+
+                    case let .failure:
+                        break
+
+                }
+            }
+        }
+
+        from.present(vc, animated: true) {
+            vc.configureRecordButton(recodeButtonAction)
+        }
+    }
+
+}
