bb6581fe77fa94e4b1806a5e2f8c136b1254e9a3
kawano108 <tk108gabalian@gmail.com>
2021-04-23 18:12:49 +0900

    #43 不要なボタンを削除

--- RockMap/View/CourseDetail/Cells/CompleteButtonCollectionViewCell.swift
+++ RockMap/View/CourseDetail/Cells/CompleteButtonCollectionViewCell.swift
@@ -10,23 +10,10 @@ import UIKit
 class CompleteButtonCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var completeButton: UIButton!
-    @IBOutlet weak var bookMarkButton: UIButton!
-    
-    var isBookMarked: Bool = false {
-        didSet {
-            bookMarkButton.setImage(
-                isBookMarked ? UIImage.SystemImages.bookMark : UIImage.SystemImages.bookMarkFill,
-                for: .normal
-            )
-        }
-    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
 
         completeButton.layer.cornerRadius = 8
-        bookMarkButton.layer.borderWidth = 0.5
-        bookMarkButton.layer.borderColor = UIColor.Pallete.primaryGreen.cgColor
-        bookMarkButton.layer.cornerRadius = 22
     }
 }
