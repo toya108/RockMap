bb0d9bfdf720d7b115af591545218646a22092f0
kawano108 <tk108gabalian@gmail.com>
2021-04-25 00:38:52 +0900

    #43 floatingPanelの背景色修正

--- RockMap/View/RockSearch/RockAnnotationTableView/RockAnnotationsTableViewController.swift
+++ RockMap/View/RockSearch/RockAnnotationTableView/RockAnnotationsTableViewController.swift
@@ -34,6 +34,7 @@ class RockAnnotationsTableViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.backgroundColor = .systemGroupedBackground
         setupTableView()
         datasource = configureDatasource()
         setupSections()
