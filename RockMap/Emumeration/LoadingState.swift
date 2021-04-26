93834613302cead9f9dd0058366da457077e4cb0
kawano108 <tk108gabalian@gmail.com>
2021-04-26 22:11:42 +0900

    #120 loadingStateをequatableに準拠

--- RockMap/Emumeration/LoadingState.swift
+++ RockMap/Emumeration/LoadingState.swift
@@ -5,7 +5,17 @@
 //  Created by TOUYA KAWANO on 2021/04/20.
 //
 
-enum LoadingState {
+enum LoadingState: Equatable {
+    static func == (lhs: LoadingState, rhs: LoadingState) -> Bool {
+        switch (lhs, rhs) {
+            case (.stanby, .stanby), (.loading, .loading), (.finish, .finish), (.failure, failure):
+                return true
+
+            default:
+                return false
+        }
+    }
+
     case stanby
     case loading
     case finish
