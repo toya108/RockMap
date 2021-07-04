2b6542a7047c5648f18a4f97668d201a79103b04
toya108 <tk108gabalian@gmail.com>
2021-07-04 21:43:12 +0900

    #188: fix url

--- RockMap/Resources/Const/URL.swift
+++ RockMap/Resources/Const/URL.swift
@@ -9,7 +9,7 @@ import Foundation
 
 extension Resources.Const {
     struct Url {
-        static let privacyPolicy = URL(string: "https://kawano108.github.io/RockMap/PrivacyPolicy/jp")!
-        static let terms = URL(string: "https://kawano108.github.io/RockMap/Terms/jp")!
+        static let privacyPolicy = URL(string: "https://toya108.github.io/RockMap/PrivacyPolicy/jp")!
+        static let terms = URL(string: "https://toya108.github.io/RockMap/Terms/jp")!
     }
 }
