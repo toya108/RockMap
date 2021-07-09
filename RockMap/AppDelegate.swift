beed83523b72db85d5cd636a37180a089dc3fc19
toya108 <tk108gabalian@gmail.com>
2021-07-10 00:22:49 +0900

    #191: fix to be able to auth by Facebook

--- RockMap/AppDelegate.swift
+++ RockMap/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Firebase
 import FirebaseUI
+import FBSDKLoginKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        FBSDKCoreKit.ApplicationDelegate.shared.application(
+            application,
+            didFinishLaunchingWithOptions: launchOptions
+        )
         setupFirebase()
         setupRemoteConfig()
         setupPermissionOfLocation()
