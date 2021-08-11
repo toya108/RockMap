d96923472c500694a6c0cb2c6723b36900602433
toya108 <tk108gabalian@gmail.com>
2021-08-12 01:16:40 +0900

    #219: make get courses request

---
+++ DataLayer/Sources/DataLayer/Firebase/FireStore/Request/GetCoursesRequest.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+public extension FS.Request {
+    struct GetCourses: FirestoreRequestProtocol {
+
+        public typealias Entry = FSQuery
+
+        public typealias Collection = FS.Collection.Users
+        public typealias Response = [FS.Document.User]
+        public struct Parameters: Codable {
+            let userId: String
+
+            public init(userId: String) {
+                self.userId = userId
+            }
+        }
+
+        public var method: FirestoreMethod { .get }
+        public var parameters: Parameters
+        public var testDataPath: URL?
+        public var path: String { "" }
+        public var entry: Entry {
+            Collection.group.whereField("registeredUserId", in: [parameters.userId])
+        }
+
+        public init(parameters: Parameters) {
+            self.parameters = parameters
+        }
+
+    }
+}
--- DataLayer/Sources/DataLayer/Repository/Repositories.swift
+++ DataLayer/Sources/DataLayer/Repository/Repositories.swift
@@ -3,4 +3,7 @@ public struct Repositories {
     public struct User {
         public typealias Get = Repository<FS.Request.GetUser>
     }
+    public struct Course {
+        public typealias Get = Repository<FS.Request.GetCourses>
+    }
 }
