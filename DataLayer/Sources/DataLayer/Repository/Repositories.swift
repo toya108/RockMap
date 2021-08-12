361922c179d18579705c5ce3a9e53d8178d40094
toya108 <tk108gabalian@gmail.com>
2021-08-13 01:14:01 +0900

    #219: add set course repository

--- DataLayer/Sources/DataLayer/Repository/Repositories.swift
+++ DataLayer/Sources/DataLayer/Repository/Repositories.swift
@@ -6,5 +6,6 @@ public struct Repositories {
     public struct Course {
         public typealias FetchByUserId = Repository<FS.Request.FetchByUserId>
         public typealias FetchByRockId = Repository<FS.Request.FetchByRockId>
+        public typealias Set = Repository<FS.Request.Set>
     }
 }
