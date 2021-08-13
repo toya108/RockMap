c30ddc3bc3c54392b73bd78823cb3316db2a857f
toya108 <tk108gabalian@gmail.com>
2021-08-13 20:45:37 +0900

    #219: make set course usecase

---
+++ Domain/Sources/Domain/Usecase/Course/Set.swift
@@ -0,0 +1,35 @@
+import Combine
+import DataLayer
+
+public extension Domain.Usecase.Course {
+    struct Set: UsecaseProtocol {
+        public typealias Repository = Repositories.Course.Set
+        public typealias Mapper = Domain.Mapper.Course
+
+        var repository: Repository
+        var mapper: Mapper
+
+        public init(repository: Repository = .init(), mapper: Mapper = .init()) {
+            self.repository = repository
+            self.mapper = mapper
+        }
+
+        public func set(course: Domain.Entity.Course) -> AnyPublisher<Void, Error> {
+            self.toPublisher { promise in
+
+                let document = mapper.reverse(to: course)
+
+                repository.request(parameters: .init(course: document)) { result in
+                    switch result {
+                        case .success:
+                            promise(.success(()))
+
+                        case let .failure(error):
+                            promise(.failure(error))
+                    }
+                }
+            }
+        }
+
+    }
+}
