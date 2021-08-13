7e1978512570cd769a1862382359d9c4deb44426
toya108 <tk108gabalian@gmail.com>
2021-08-14 00:01:16 +0900

    #219: fix path builder

--- DataLayer/Sources/DataLayer/Firebase/FireStore/Core/PathBuilder.swift
+++ DataLayer/Sources/DataLayer/Firebase/FireStore/Core/PathBuilder.swift
@@ -17,10 +17,12 @@ struct PathBuilder {
         type(of: expression).name
     }
 
-    static func buildExpression(_ expression: DocumentProtocol) -> [String] {
-        expression.collection.isRoot
+    static func buildExpression(_ expression: DocumentProtocol) -> String {
+        let strings = expression.collection.isRoot
             ? [expression.collection.name, expression.id]
             : [expression.parentPath, expression.collection.name, expression.id]
+
+        return strings.joined(separator: "/")
     }
 
     static func buildFinalResult(_ component: [String]) -> String {
