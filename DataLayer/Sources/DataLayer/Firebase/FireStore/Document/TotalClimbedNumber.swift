93293fa70c97e9095a08ab6cd0bce8fa4acc5df7
toya108 <tk108gabalian@gmail.com>
2021-08-16 20:39:14 +0900

    #219: make listen to totalClimbedNumber

--- DataLayer/Sources/DataLayer/Firebase/FireStore/Document/TotalClimbedNumber.swift
+++ DataLayer/Sources/DataLayer/Firebase/FireStore/Document/TotalClimbedNumber.swift
@@ -2,18 +2,19 @@
 import Foundation
 import FirebaseFirestoreSwift
 
-extension FS.Document {
+public extension FS.Document {
 
     struct TotalClimbedNumber: DocumentProtocol {
 
-        var collection: CollectionProtocol.Type { FS.Collection.TotalClimbedNumber.self }
+        public var collection: CollectionProtocol.Type { FS.Collection.TotalClimbedNumber.self }
 
-        var id: String
-        var createdAt: Date = Date()
-        @ExplicitNull var updatedAt: Date?
-        var parentPath: String
-        var flash: Int
-        var redPoint: Int
+        public var id: String
+        public var createdAt: Date = Date()
+        @ExplicitNull
+        public var updatedAt: Date?
+        public var parentPath: String
+        public var flash: Int
+        public var redPoint: Int
     }
     
 }
---
+++ DataLayer/Sources/DataLayer/Firebase/FireStore/Request/TotalClimbedNumber/ListenByCourseId.swift
@@ -0,0 +1,39 @@
+
+import Foundation
+
+public extension FS.Request {
+    struct ListenByCourseId: FirestoreRequestProtocol {
+
+        public typealias Entry = FSQuery
+
+        public typealias Collection = FS.Collection.TotalClimbedNumber
+        public typealias Response = FS.Document.TotalClimbedNumber
+        public struct Parameters: Codable {
+            let parentPath: String
+            let courseId: String
+
+            public init(parentPath: String, courseId: String) {
+                self.parentPath = parentPath
+                self.courseId = courseId
+            }
+        }
+
+        public var method: FirestoreMethod { .listen }
+        public var parameters: Parameters
+        public var testDataPath: URL?
+        public var path: String {
+            parameters.parentPath
+            FS.Collection.Courses.name
+            parameters.courseId
+        }
+        public var entry: Entry {
+            FirestoreManager.db.document(path).collection(Collection.name)
+        }
+
+        public init(parameters: Parameters) {
+            self.parameters = parameters
+        }
+
+    }
+}
+
