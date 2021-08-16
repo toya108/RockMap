74b60ee03ff01f5ce0962d01358441e2c0b0edbe
toya108 <tk108gabalian@gmail.com>
2021-08-16 22:20:55 +0900

    #219: fix documentTypes

--- DataLayer/Sources/DataLayer/Firebase/FireStore/Collections.swift
+++ DataLayer/Sources/DataLayer/Firebase/FireStore/Collections.swift
@@ -9,37 +9,49 @@ public protocol CollectionProtocol {
 
 public extension CollectionProtocol {
     static var isRoot: Bool { false }
-    static var group: FSQuery { FirestoreManager.db.collectionGroup(Self.name) }
+    static var group: FSQuery {
+        FirestoreManager.db.collectionGroup(Self.name)
+    }
 }
 
 public extension FS.Collection {
     struct Users: CollectionProtocol {
-        public static var Document: DocumentProtocol.Type { FS.Document.User.self }
+        public static var Document: DocumentProtocol.Type {
+            FS.Document.User.self
+        }
 
         public static var name: String { "users" }
         public static var isRoot: Bool { true }
     }
 
     struct Rocks: CollectionProtocol {
-        public static var Document: DocumentProtocol.Type { FS.Document.User.self }
+        public static var Document: DocumentProtocol.Type {
+            FS.Document.Rock.self
+        }
 
         public static var name: String { "users" }
     }
 
     struct Courses: CollectionProtocol {
-        public static var Document: DocumentProtocol.Type { FS.Document.User.self }
+        public static var Document: DocumentProtocol.Type {
+            FS.Document.Course.self
+        }
 
         public static var name: String { "courses" }
     }
 
     struct ClimbRecord: CollectionProtocol {
-        public static var Document: DocumentProtocol.Type { FS.Document.User.self }
+        public static var Document: DocumentProtocol.Type {
+            FS.Document.ClimbRecord.self
+        }
 
         public static var name: String { "climbRecord" }
     }
 
     struct TotalClimbedNumber: CollectionProtocol {
-        public static var Document: DocumentProtocol.Type { FS.Document.User.self }
+        public static var Document: DocumentProtocol.Type {
+            FS.Document.TotalClimbedNumber.self
+        }
 
         public static var name: String { "totalClimbedNumber" }
     }
