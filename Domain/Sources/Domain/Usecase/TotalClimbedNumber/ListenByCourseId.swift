9f6b1b60c33c0676d78c5d8744490aa480d16cb0
toya108 <tk108gabalian@gmail.com>
2021-08-18 09:33:24 +0900

    #219: fix totalNumber usecase to listen to query

--- Domain/Sources/Domain/Usecase/TotalClimbedNumber/ListenByCourseId.swift
+++ Domain/Sources/Domain/Usecase/TotalClimbedNumber/ListenByCourseId.swift
@@ -3,44 +3,36 @@ import Combine
 import DataLayer
 
 public extension Domain.Usecase.TotalClimbedNumber {
-    class FetchById: UsecaseProtocol, Listenable {
+    struct ListenByCourseId: UsecaseProtocol, Listenable {
+
         public typealias Repository = Repositories.TotalClimbedNumber.ListenByCourseId
         public typealias Mapper = Domain.Mapper.TotalClimbedNumber
 
         var repository: Repository
         var mapper: Mapper
 
-        var registration: FSListenerRegistration?
-
-        required public init(repository: Repository = .init(), mapper: Mapper = .init()) {
+        public init(repository: Repository = .init(), mapper: Mapper = .init()) {
             self.repository = repository
             self.mapper = mapper
         }
 
-        public func listen(parentPath: String, courseId: String) -> AnyPublisher<Domain.Entity.TotalClimbedNumber, Error> {
-            self.toPublisher { [weak self] promise in
+        public func listen(
+            useTestData: Bool,
+            courseId: String,
+            parantPath: String
+        ) -> AnyPublisher<Domain.Entity.TotalClimbedNumber, Error> {
+            Domain.Usecase.ListenPublisher(
+                repository: repository,
+                useTestData: useTestData,
+                paremeters: .init(parentPath: parantPath, courseId: courseId)
+            )
+            .compactMap { response -> Domain.Entity.TotalClimbedNumber? in
 
-                guard let self = self else { return }
+                guard let document = response.first else { return nil }
 
-                self.registration = self.repository.listen(
-                    parameters: .init(parentPath: parentPath, courseId: courseId)
-                ) { [weak self] result in
-
-                    guard let self = self else { return }
-
-                    switch result {
-                        case let .success(response):
-
-                            guard let document = response.first else { return }
-
-                            let entity = self.mapper.map(from: document)
-                            promise(.success(entity))
-
-                        case let .failure(error):
-                            promise(.failure(error))
-                    }
-                }
+                return self.mapper.map(from: document)
             }
+            .eraseToAnyPublisher()
         }
     }
 }
