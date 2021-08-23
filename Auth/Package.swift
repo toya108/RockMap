a9c5b9dfa3827fa3d777b7afc1125419546107fc
toya108 <tk108gabalian@gmail.com>
2021-08-23 23:23:05 +0900

    #219: add auth package

---
+++ Auth/Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Auth",
+    platforms: [
+        .iOS(.v14),
+    ],
+    products: [
+        .library(
+            name: "Auth",
+            targets: ["Auth"]),
+    ],
+    dependencies: [
+        .package(
+            name: "FirebaseUI",
+            url: "https://github.com/firebase/FirebaseUI-iOS.git",
+            from: .init("12.0.0")
+        ),
+        .package(url: "../Utilities", from: "1.0.0")
+    ],
+    targets: [
+        .target(
+            name: "Auth",
+            dependencies: [
+                .product(name: "FirebaseAuthUI", package: "FirebaseUI"),
+                .product(name: "FirebaseGoogleAuthUI", package: "FirebaseUI"),
+                .product(name: "FirebaseEmailAuthUI", package: "FirebaseUI"),
+                .product(name: "FirebaseOAuthUI", package: "FirebaseUI")
+            ]),
+        .testTarget(
+            name: "AuthTests",
+            dependencies: ["Auth"]),
+    ]
+)
