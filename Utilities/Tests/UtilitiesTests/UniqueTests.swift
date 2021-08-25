958b18d105748124df2bd5143287bae2d78e46db
toya108 <tk108gabalian@gmail.com>
2021-08-26 00:26:58 +0900

    #219: add unique test

---
+++ Utilities/Tests/UtilitiesTests/UniqueTests.swift
@@ -0,0 +1,17 @@
+
+import XCTest
+@testable import Utilities
+
+class UniqueTests: XCTestCase {
+
+    func testUnique() {
+        XCTAssertEqual([1, 1, 1, 1].unique, [1])
+
+        XCTAssertEqual([1, 2, 1, 1].unique, [1, 2])
+
+        XCTAssertEqual([1, 2, 3, 1].unique, [1, 2, 3])
+
+        XCTAssertEqual([1, 2, 3, 4].unique, [1, 2, 3, 4])
+    }
+
+}
