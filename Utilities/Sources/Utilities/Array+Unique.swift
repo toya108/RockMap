f9e551c45644ad50162a1a6e9f65ce1d23d843c6
toya108 <tk108gabalian@gmail.com>
2021-08-26 00:17:24 +0900

    #219: add unique to array

---
+++ Utilities/Sources/Utilities/Array+Unique.swift
@@ -0,0 +1,12 @@
+
+import Foundation
+
+public extension Array where Element: Hashable {
+    var unique: [Element] {
+        self.reduce([]) {
+            $0.contains($1)
+                ? $0
+                : $0 + [$1]
+        }
+    }
+}
