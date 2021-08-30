2018c818af4266ad6b641c56952fb4496b0dde0c
toya108 <tk108gabalian@gmail.com>
2021-08-30 23:27:12 +0900

    #219: optimization

---
+++ DataLayer/Sources/DataLayer/Firebase/Extensions/WriteBadge+Combine.swift
@@ -0,0 +1,28 @@
+import Combine
+import FirebaseFirestore
+
+extension WriteBatch {
+
+    func commit() -> AnyPublisher<Void, Error> {
+
+        Deferred {
+            Future<Void, Error> { [weak self] promise in
+
+                guard let self = self else { return }
+
+                self.commit { error in
+
+                    if let error = error {
+                        promise(.failure(error))
+                        return
+                    }
+
+                    promise(.success(()))
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+
+    }
+
+}
