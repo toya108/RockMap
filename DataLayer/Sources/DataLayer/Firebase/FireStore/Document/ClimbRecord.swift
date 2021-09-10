5dea46f4c56127a8de60c4f910cf6a6009c01fb6
toya108 <tk108gabalian@gmail.com>
2021-09-10 21:45:20 +0900

    #246: fix totalNumber to be optional

--- DataLayer/Sources/DataLayer/Firebase/FireStore/Document/ClimbRecord.swift
+++ DataLayer/Sources/DataLayer/Firebase/FireStore/Document/ClimbRecord.swift
@@ -3,18 +3,42 @@ import Foundation
 
 public extension FS.Document {
     struct ClimbRecord: DocumentProtocol, UserRegisterableDocumentProtocol {
+
         public var collection: CollectionProtocol.Type { FS.Collection.ClimbRecord.self }
 
         public var id: String
         public var registeredUserId: String
         public var parentCourseId: String
         public var parentCourseReference: FSDocument
-        public var totalNumberReference: FSDocument
+        @ExplicitNull
+        public var totalNumberReference: FSDocument?
         public var createdAt: Date
         @ExplicitNull
         public var updatedAt: Date?
         public var parentPath: String
         public var climbedDate: Date
         public var type: String
+
+        public init(
+            id: String,
+            registeredUserId: String,
+            parentCourseId: String,
+            parentCourseReference: FSDocument,
+            createdAt: Date,
+            updatedAt: Date?,
+            parentPath: String,
+            climbedDate: Date,
+            type: String
+        ) {
+            self.id = id
+            self.registeredUserId = registeredUserId
+            self.parentCourseId = parentCourseId
+            self.parentCourseReference = parentCourseReference
+            self.createdAt = createdAt
+            self.updatedAt = updatedAt
+            self.parentPath = parentPath
+            self.climbedDate = climbedDate
+            self.type = type
+        }
     }
 }
