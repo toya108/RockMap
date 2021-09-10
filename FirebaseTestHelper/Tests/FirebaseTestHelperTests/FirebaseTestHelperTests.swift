df790620a6ef1b0a832488922b1979fe353632f0
toya108 <tk108gabalian@gmail.com>
2021-09-10 20:52:33 +0900

    #246: make firebaseTestHelper

---
+++ FirebaseTestHelper/Tests/FirebaseTestHelperTests/FirebaseTestHelperTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import FirebaseTestHelper
+
+    final class FirebaseTestHelperTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(FirebaseTestHelper().text, "Hello, World!")
+        }
+    }
