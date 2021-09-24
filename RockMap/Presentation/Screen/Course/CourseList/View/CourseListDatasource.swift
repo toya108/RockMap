b73d39e7b967f51096773e32b2694a56abd2d1f4
toya108 <tk108gabalian@gmail.com>
2021-09-24 09:28:39 +0900

    #251: fix to use local variable

--- RockMap/Presentation/Screen/Course/CourseList/View/CourseListDatasource.swift
+++ RockMap/Presentation/Screen/Course/CourseList/View/CourseListDatasource.swift
@@ -2,49 +2,48 @@ import UIKit
 
 extension CourseListViewController {
     func configureDatasource() -> UICollectionViewDiffableDataSource<SectionKind, ItemKind> {
+
+        let headerCellRegistration = UICollectionView.CellRegistration<
+            AnnotationHeaderCollectionViewCell,
+            Dummy
+        > { cell, _, _ in
+            cell.configure(title: "課題を長押しすると編集/削除ができます。")
+        }
+
+        let courseCellRegistration = UICollectionView.CellRegistration<
+            ListCollectionViewCell,
+            Entity.Course
+        >(
+            cellNib: .init(
+                nibName: ListCollectionViewCell.className,
+                bundle: nil
+            )
+        ) { cell, _, course in
+            cell.configure(
+                imageUrl: course.headerUrl,
+                iconImage: UIImage.SystemImages.docPlaintextFill,
+                title: course.name + " " + course.grade.name,
+                first: "登録日: " + course.createdAt.string(dateStyle: .medium),
+                second: "岩名: " + course.parentRockName,
+                third: course.desc
+            )
+        }
+
         let datasource = UICollectionViewDiffableDataSource<SectionKind, ItemKind>(
             collectionView: collectionView
-        ) { [weak self] _, indexPath, item in
-
-            guard let self = self else { return UICollectionViewCell() }
-
-            let headerCellRegistration = UICollectionView.CellRegistration<
-                AnnotationHeaderCollectionViewCell,
-                Dummy
-            > { cell, _, _ in
-                cell.configure(title: "課題を長押しすると編集/削除ができます。")
-            }
-
-            let courseCellRegistration = UICollectionView.CellRegistration<
-                ListCollectionViewCell,
-                Entity.Course
-            >(
-                cellNib: .init(
-                    nibName: ListCollectionViewCell.className,
-                    bundle: nil
-                )
-            ) { cell, _, course in
-                cell.configure(
-                    imageUrl: course.headerUrl,
-                    iconImage: UIImage.SystemImages.docPlaintextFill,
-                    title: course.name + " " + course.grade.name,
-                    first: "登録日: " + course.createdAt.string(dateStyle: .medium),
-                    second: "岩名: " + course.parentRockName,
-                    third: course.desc
-                )
-            }
+        ) { collectionView, indexPath, item in
 
             switch item {
             case .annotationHeader:
-                return self.collectionView.dequeueConfiguredReusableCell(
-                    using: self.registrations.headerCellRegistration,
+                return collectionView.dequeueConfiguredReusableCell(
+                    using: headerCellRegistration,
                     for: indexPath,
                     item: Dummy()
                 )
 
             case let .course(course):
-                return self.collectionView.dequeueConfiguredReusableCell(
-                    using: self.registrations.courseCellRegistration,
+                return collectionView.dequeueConfiguredReusableCell(
+                    using: courseCellRegistration,
                     for: indexPath,
                     item: course
                 )
