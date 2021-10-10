b96f382013cb9dc7fec5696f9fb15762e377f9c0
toya108 <tk108gabalian@gmail.com>
2021-10-10 23:58:04 +0900

    replace cli tools to spm

---
+++ Tools/RockMapTools/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RockMapTools",
+    platforms: [
+        .iOS(.v15)
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/mono0926/LicensePlist",
+            from: .init("3.0.7")
+        ),
+        .package(
+            url: "https://github.com/realm/SwiftLint",
+            from: .init("0.44.0")
+        )
+    ],
+    targets: [
+        .target(name: "RockMapTools", path: "")
+    ]
+)
