38982ce71fdcc2c6c3b35b74e4e047289f4beabf
toya108 <tk108gabalian@gmail.com>
2021-11-19 22:18:32 +0900

    add an async method to write batch

--- DataLayer/Sources/DataLayer/Firebase/Extensions/WriteBadge+Combine.swift
+++ DataLayer/Sources/DataLayer/Firebase/Extensions/WriteBadge+Combine.swift
@@ -22,3 +22,23 @@ extension WriteBatch {
         .eraseToAnyPublisher()
     }
 }
+
+extension WriteBatch {
+    func commit() async throws -> EmptyResponse {
+        try await withCheckedThrowingContinuation { [weak self] continuation in
+
+            guard let self = self else { return }
+
+            self.commit { error in
+
+                if let error = error {
+                    continuation.resume(throwing: error)
+                    return
+                }
+
+                continuation.resume(returning: .init())
+            }
+        }
+    }
+}
+
