28e9a25d2540e96147a939d41163e9a8ead42ef4
toya108 <tk108gabalian@gmail.com>
2021-12-20 23:50:33 +0900

    #278 fix socialLink button color

--- RockMap/Model/Domain/Enitty.User.SocialLinkType.Icon.swift
+++ RockMap/Model/Domain/Enitty.User.SocialLinkType.Icon.swift
@@ -29,7 +29,7 @@ extension Entity.User.SocialLinkType {
             return UIColor.Pallete.instagram
 
         case .other:
-            return .black
+            return .label
         }
     }
 }
