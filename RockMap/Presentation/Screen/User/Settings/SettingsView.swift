f7a1bb4e9d9c7ff2db91bc719accebf5a40186bd
toya108 <tk108gabalian@gmail.com>
2021-12-24 22:40:03 +0900

    #278: make settings view

--- RockMap/Presentation/Screen/User/Settings/SettingsView.swift
+++ RockMap/Presentation/Screen/User/Settings/SettingsView.swift
@@ -1,15 +1,65 @@
-//
-//  SettingsView.swift
-//  RockMap
-//
-//  Created by TOUYA KAWANO on 2021/12/22.
-//
-
 import SwiftUI
+import StoreKit
 
 struct SettingsView: View {
+
+    @State private var shouldShowPrivacyPolicy = false
+    @State private var shouldShowTerm = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                Section(header: Text("アカウント")) {
+                    NavigationLink(
+                        destination: {
+                            AccountView()
+                        },
+                        label: {
+                            HStack {
+                                Image(uiImage: UIImage.SystemImages.personCircle)
+                                Text("アカウント")
+                            }
+                        }
+                    )
+                }
+                Section(header: Text("このアプリについて")) {
+                    HStack {
+                        Image(uiImage: UIImage.SystemImages.checkmarkShield)
+                        Text("プライバシーポリシー").onTapGesture {
+                            shouldShowPrivacyPolicy.toggle()
+                        }
+                    }
+                    HStack {
+                        Image(uiImage: UIImage.SystemImages.docPlaintext)
+                        Text("利用規約").onTapGesture {
+                            shouldShowPrivacyPolicy.toggle()
+                        }
+                    }
+                    HStack {
+                        Image(uiImage: UIImage.SystemImages.starCircle)
+                        Text("レビュー").onTapGesture {
+                            guard
+                                let scene = UIApplication.shared.connectedScenes.first(
+                                    where: { $0.activationState == .foregroundActive }
+                                ) as? UIWindowScene
+                            else {
+                                return
+                            }
+                            SKStoreReviewController.requestReview(in: scene)
+                        }
+                    }
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("設定")
+        }
+        .sheet(isPresented: $shouldShowPrivacyPolicy) {
+            SafariView(url: Resources.Const.Url.privacyPolicy)
+        }
+        .sheet(isPresented: $shouldShowTerm) {
+            SafariView(url: Resources.Const.Url.terms)
+        }
+
     }
 }
 
