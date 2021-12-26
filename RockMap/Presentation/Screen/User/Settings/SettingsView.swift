edb044169c3c6fed37e87642bcdc6a500ff54634
toya108 <tk108gabalian@gmail.com>
2021-12-26 15:57:18 +0900

    #278 enable to tap entire cell

--- RockMap/Presentation/Screen/User/Settings/SettingsView.swift
+++ RockMap/Presentation/Screen/User/Settings/SettingsView.swift
@@ -11,9 +11,7 @@ struct SettingsView: View {
             List {
                 Section(header: Text("account")) {
                     NavigationLink(
-                        destination: {
-                            AccountView()
-                        },
+                        destination: AccountView(),
                         label: {
                             HStack {
                                 Image(uiImage: UIImage.SystemImages.personCircle)
@@ -25,41 +23,54 @@ struct SettingsView: View {
                 Section(header: Text("about_this_app")) {
                     HStack {
                         Image(uiImage: UIImage.SystemImages.checkmarkShield)
-                        Text("privacy_policy").onTapGesture {
-                            shouldShowPrivacyPolicy.toggle()
-                        }
+                        Text("privacy_policy")
+                        Spacer()
+                    }
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        shouldShowPrivacyPolicy.toggle()
                     }
                     HStack {
                         Image(uiImage: UIImage.SystemImages.docPlaintext)
-                        Text("terms").onTapGesture {
-                            shouldShowPrivacyPolicy.toggle()
-                        }
+                        Text("terms")
+                        Spacer()
+                    }
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        shouldShowPrivacyPolicy.toggle()
                     }
                     HStack {
                         Image(uiImage: UIImage.SystemImages.starCircle)
-                        Text("review").onTapGesture {
-                            guard
-                                let scene = UIApplication.shared.connectedScenes.first(
-                                    where: { $0.activationState == .foregroundActive }
-                                ) as? UIWindowScene
-                            else {
-                                return
-                            }
-                            SKStoreReviewController.requestReview(in: scene)
-                        }
+                        Text("review")
+                        Spacer()
+                    }
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        showStoreReview()
                     }
                 }
             }
             .navigationBarTitleDisplayMode(.inline)
             .navigationTitle("settings")
         }
+        .navigationViewStyle(StackNavigationViewStyle())
         .sheet(isPresented: $shouldShowPrivacyPolicy) {
             SafariView(url: Resources.Const.Url.privacyPolicy)
         }
         .sheet(isPresented: $shouldShowTerm) {
             SafariView(url: Resources.Const.Url.terms)
         }
+    }
 
+    private func showStoreReview() {
+        guard
+            let scene = UIApplication.shared.connectedScenes.first(
+                where: { $0.activationState == .foregroundActive }
+            ) as? UIWindowScene
+        else {
+            return
+        }
+        SKStoreReviewController.requestReview(in: scene)
     }
 }
 
