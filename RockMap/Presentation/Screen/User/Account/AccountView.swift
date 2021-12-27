046320df3d9be6f9aba593702305e98dbc62202e
toya108 <tk108gabalian@gmail.com>
2021-12-27 10:36:33 +0900

    #278 add account view

---
+++ RockMap/Presentation/Screen/User/Account/AccountView.swift
@@ -0,0 +1,111 @@
+import SwiftUI
+import Resolver
+
+struct AccountView: View {
+
+    @StateObject var viewModel: AccountViewModel = Resolver.resolve()
+
+    var body: some View {
+        NavigationView {
+            List {
+                Section(header: Text("manage_account")) {
+                    HStack {
+                        Text("id")
+                        Spacer()
+                        Text(viewModel.authAccessor.uid)
+                    }
+                    HStack {
+                        Text("how_to_login")
+                        Spacer()
+                        Text(viewModel.authAccessor.providerID)
+                    }
+                }
+                Section(header: Text("user_information")) {
+                    EntireTappableRowView {
+                        Text(viewModel.authAccessor.isLoggedIn ? "logout" : "login")
+                    }
+                    .onTapGesture {
+                        if viewModel.authAccessor.isLoggedIn {
+                            viewModel.isPresentedLogoutAlert = true
+                        } else {
+                            AppStore.shared.rootViewType = .login
+                        }
+                    }
+
+                    if viewModel.authAccessor.isLoggedIn {
+                        EntireTappableRowView {
+                            Text("delete_account")
+                        }
+                        .onTapGesture {
+                            viewModel.isPresentedDeleteAccountAlert = true
+                        }
+                    }
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("account")
+            .onReceive(viewModel.$shouldChangeRootToLogin) {
+                if $0 {
+                    AppStore.shared.rootViewType = .login
+                }
+            }
+            .alert(
+                "would_you_like_to_delete_your_account",
+                isPresented: $viewModel.isPresentedDeleteAccountAlert,
+                actions: {
+                    Button("ok", role: .destructive) {
+                        viewModel.deleteAccount()
+                    }
+                    Button("cancel", role: .cancel) {
+                        viewModel.isPresentedDeleteAccountAlert = false
+                    }
+                },
+                message: {
+                    Text("delete_account_alert_message")
+                }
+            )
+            .alert(
+                "account_deleting_failed",
+                isPresented: $viewModel.isPresentedDeleteAccountFailureAlert,
+                actions: {
+                    Button("ok") {}
+                },
+                message: {
+                    Text(viewModel.deleteAccountError?.localizedDescription ?? "")
+                }
+            )
+            .alert(
+                "text_logout_question",
+                isPresented: $viewModel.isPresentedLogoutAlert,
+                actions: {
+                    Button("yes") {
+                        viewModel.logout()
+                    }
+                    Button("cancel", role: .cancel) {
+                        viewModel.isPresentedLogoutAlert = false
+                    }
+                },
+                message: {
+                    Text("logout_alert_message")
+                }
+            )
+            .alert(
+                "text_logout_failure",
+                isPresented: $viewModel.isPresentedLogoutFailureAlert,
+                actions: {
+                    Button("ok") {}
+                },
+                message: {
+                    Text(viewModel.logoutError?.localizedDescription ?? "")
+                }
+            )
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+struct AccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountView()
+    }
+}
