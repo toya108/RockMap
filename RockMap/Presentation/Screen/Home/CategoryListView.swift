c3fcf8c05893e1b00aaf8e7f8e06948184939c3e
toya108 <tk108gabalian@gmail.com>
2022-01-26 00:22:28 +0900

    configure CategoryList

--- RockMap/Presentation/Screen/Home/CategoryListView.swift
+++ RockMap/Presentation/Screen/Home/CategoryListView.swift
@@ -5,20 +5,27 @@ struct CategoryListView: View {
     @Binding var selectedCategory: CategoryKind
 
     var body: some View {
-        VStack {
-            Picker("カテゴリ", selection: $selectedCategory) {
-                ForEach(CategoryKind.allCases) {
-                    Text($0.name).tag($0)
-                }
-            }
-            .pickerStyle(.segmented)
-            ScrollView {
-                HStack {
+        GeometryReader { geometry in
+            VStack {
+                Picker("", selection: $selectedCategory) {
                     ForEach(CategoryKind.allCases) {
-                        Text($0.name)
+                        Text($0.name).tag($0)
                     }
                 }
+                .pickerStyle(.segmented)
+                TabView(selection: $selectedCategory) {
+                    ForEach(CategoryKind.allCases) {
+                        $0.view.tag($0).frame(width: geometry.size.width)
+                    }
+                }
+                .tabViewStyle(PageTabViewStyle())
             }
+        }
+        .onAppear {
+            UISegmentedControl.appearance().selectedSegmentTintColor = UIColor.Pallete.primaryGreen
+            UISegmentedControl.appearance().setTitleTextAttributes(
+                [.foregroundColor: UIColor.white], for: .selected
+            )
         }
     }
 }
