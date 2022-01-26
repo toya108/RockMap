f6cce8bbf28c48c6877374874be21fe0067223d4
toya108 <tk108gabalian@gmail.com>
2022-01-26 23:54:59 +0900

    add animation to searchView

--- RockMap/Presentation/Screen/Home/SearchView.swift
+++ RockMap/Presentation/Screen/Home/SearchView.swift
@@ -4,26 +4,26 @@ struct SearchView: View {
 
     @Binding var searchText: String
     @FocusState var isFocusedSearchField: Bool
-    let didTapResetButton: () -> Void
+    @State private var shouldShowBackButton: Bool = false
 
     var body: some View {
         HStack {
-            HStack {
-                Image(systemName: "magnifyingglass")
-                TextField("なんか", text: $searchText)
-                    .focused($isFocusedSearchField)
+            if shouldShowBackButton {
                 Button(
-                    action: didTapResetButton,
+                    action: {
+                        isFocusedSearchField = false
+                    },
                     label: {
-                        Image(systemName: "xmark.circle.fill")
-                            .opacity(searchText.isEmpty ? 0 : 1)
+                        let chevronCompactLeft = UIImage.SystemImages.chevronCompactLeft
+                        Image(uiImage: chevronCompactLeft.withRenderingMode(.alwaysTemplate))
+                            .foregroundColor(.primary)
                     }
                 )
             }
-            .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
-            .foregroundColor(.secondary)
-            .background(Color(.secondarySystemBackground))
-            .cornerRadius(8)
+            SearchTextField(
+                searchText: $searchText,
+                isFocusedSearchField: _isFocusedSearchField
+            )
             Button(
                 action: {
 
@@ -34,5 +34,44 @@ struct SearchView: View {
                 }
             )
         }
+        .onChange(of: isFocusedSearchField) { isFocused in
+            withAnimation(.linear(duration: 0.2)) {
+                shouldShowBackButton = isFocused
+            }
+        }
+    }
+}
+
+struct SearchTextField: View {
+
+    @Binding var searchText: String
+    @FocusState var isFocusedSearchField: Bool
+
+    var body: some View {
+        HStack {
+            Button(
+                action: {
+                    isFocusedSearchField = true
+                },
+                label: {
+                    Image(systemName: "magnifyingglass")
+                }
+            )
+            TextField("なんか", text: $searchText)
+                .focused($isFocusedSearchField)
+            Button(
+                action: {
+                    searchText = ""
+                },
+                label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .opacity(searchText.isEmpty ? 0 : 1)
+                }
+            )
+        }
+        .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
+        .foregroundColor(.secondary)
+        .background(Color(.secondarySystemBackground))
+        .cornerRadius(8)
     }
 }
