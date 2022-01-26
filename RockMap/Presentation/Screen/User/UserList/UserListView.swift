79c29853bf7cb03a88dab071ec5411b63bb2a374
toya108 <tk108gabalian@gmail.com>
2022-01-27 01:21:04 +0900

    add file for userList

---
+++ RockMap/Presentation/Screen/User/UserList/UserListView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct UserListView: View {
+    var body: some View {
+        List {
+            
+        }
+    }
+}
+
+struct UserListView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserListView()
+    }
+}
