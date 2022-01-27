0756a35003448d62541fc180807aae523a0f0930
toya108 <tk108gabalian@gmail.com>
2022-01-27 21:28:54 +0900

    make userList

--- RockMap/Presentation/Screen/User/UserList/UserListView.swift
+++ RockMap/Presentation/Screen/User/UserList/UserListView.swift
@@ -1,15 +1,63 @@
 import SwiftUI
 
 struct UserListView: View {
+
+    @StateObject var viewModel: UserListViewModel
+
     var body: some View {
-        List {
-            
+        switch viewModel.viewState {
+            case .standby:
+                Color.clear.onAppear {
+                    Task {
+                        await viewModel.load()
+                    }
+                }
+
+            case .loading:
+                ListSkeltonView()
+
+            case .failure:
+                EmptyView(text: .init("text_fetch_user_failed"))
+
+            case .finish:
+                if viewModel.users.isEmpty {
+                    EmptyView(text: .init("text_no_user"))
+
+                } else {
+                    List(viewModel.users) { user in
+                        NavigationLink(
+                            destination: MyPageView(userKind: .other(user: user))
+                        ) {
+                            HStack {
+                                AsyncImage(
+                                    url: user.photoURL,
+                                    content: { image in
+                                        image.resizable().scaledToFill()
+                                    },
+                                    placeholder: {
+                                        ProgressView()
+                                    }
+                                )
+                                .clipShape(Circle())
+                                .frame(width: 44, height: 44)
+                                Text(user.name)
+                            }
+                            .padding(.init(top: 8, leading: 0, bottom: 8, trailing: 0))
+                        }
+                    }
+                    .listStyle(.plain)
+                    .refreshable {
+                        Task {
+                            await viewModel.refresh()
+                        }
+                    }
+                }
         }
     }
 }
 
 struct UserListView_Previews: PreviewProvider {
     static var previews: some View {
-        UserListView()
+        UserListView(viewModel: .init())
     }
 }
--- RockMap/Presentation/Screen/User/UserList/UserListViewModel.swift
+++ RockMap/Presentation/Screen/User/UserList/UserListViewModel.swift
@@ -1,1 +1,54 @@
+import Combine
 import Foundation
+import Resolver
+import Domain
+import Collections
+
+actor UserListViewModel: ObservableObject {
+
+    @Published nonisolated var users: OrderedSet<Entity.User> = []
+    @Published nonisolated var viewState: LoadableViewState = .standby
+
+    private var page: Int = 0
+
+    @Injected private var fetchUserListUsecase: FetchUserListUsecaseProtocol
+
+    @MainActor func load() async {
+        self.viewState = .loading
+
+        do {
+            let users = try await fetchUserListUsecase.fetch(startAt: startAt)
+            self.users.append(contentsOf: users)
+            self.viewState = .finish
+        } catch {
+            self.viewState = .failure(error)
+        }
+    }
+
+    func additionalLoad() async {
+        self.page += 1
+        await self.load()
+    }
+
+    func refresh() async {
+        self.page = 0
+        await self.load()
+    }
+
+    func shouldAdditionalLoad(user: Entity.User) async -> Bool {
+        guard let index = users.firstIndex(of: user) else {
+            return false
+        }
+        return user.id == users.last?.id
+        && (Double(index) / 20.0) == 0.0
+        && index != 0
+    }
+
+    private var startAt: Date {
+        if let last = users.last {
+            return last.createdAt
+        } else {
+            return Date()
+        }
+    }
+}
