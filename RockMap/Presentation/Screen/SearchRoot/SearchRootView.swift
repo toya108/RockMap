f9ee1964f75fa7fb72fcb45b2510aa86ad926966
toya108 <tk108gabalian@gmail.com>
2022-02-01 12:07:54 +0900

    fix rendering

--- RockMap/Presentation/Screen/SearchRoot/SearchRootView.swift
+++ RockMap/Presentation/Screen/SearchRoot/SearchRootView.swift
@@ -11,18 +11,21 @@ struct SearchRootView: View {
                     isPresentedSearchFilter: $viewModel.isPresentedSearchFilter,
                     searchText: $viewModel.searchCondition.searchText,
                     isFocusedSearchField: _isFocusedSearchField
-                )
-                .padding(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16))
+                ).padding(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16))
 
-                if isFocusedSearchField && viewModel.searchCondition.searchText.isEmpty {
-                    SearchHistoryView()
-                    Spacer()
-                } else {
-                    if viewModel.searchCondition.searchText.isEmpty {
-                        CategoryListView(selectedCategory: $viewModel.selectedCategory)
-                    } else {
-                        RockSearchView(viewModel: .init(), searchCondition: viewModel.searchCondition)
+                ZStack {
+                    VStack {
+                        SearchHistoryView()
+                        Spacer()
                     }
+                    .opacity(isFocusedSearchField && viewModel.searchCondition.searchText.isEmpty ? 1 : 0)
+                    CategoryListView(selectedCategory: $viewModel.selectedCategory)
+                        .opacity(!isFocusedSearchField && viewModel.searchCondition.searchText.isEmpty ? 1 : 0)
+                    RockSearchView(
+                        viewModel: .init(),
+                        searchCondition: viewModel.searchCondition
+                    )
+                    .opacity(!isFocusedSearchField && viewModel.searchCondition.searchText.isEmpty ? 0 : 1)
                 }
             }
             .navigationBarHidden(true)
