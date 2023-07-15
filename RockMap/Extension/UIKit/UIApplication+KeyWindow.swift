17b2b78e95b54dbb4db0a1aabddd10fd4892a7f2
toya108 <tk108gabalian@gmail.com>
2023-07-16 08:55:16 +0900

    警告を解消

--- RockMap/Extension/UIKit/UIApplication+KeyWindow.swift
+++ RockMap/Extension/UIKit/UIApplication+KeyWindow.swift
@@ -4,8 +4,7 @@ extension UIApplication {
 
     var keyWindow: UIWindow? {
         return UIApplication.shared.connectedScenes
-            .filter { $0.activationState == .foregroundActive }
-            .first(where: { $0 is UIWindowScene })
+            .first(where: { $0.activationState == .foregroundActive && $0 is UIWindowScene })
             .flatMap({ $0 as? UIWindowScene })?.windows
             .first(where: \.isKeyWindow)
     }
